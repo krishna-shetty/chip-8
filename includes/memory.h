337defc17c169fe8f48e441f35d0ea4a8ebdef2c
Krishna Shetty <KrishnaHarishShetty@gmail.com>
2026-02-15 15:37:50 -0800

    Add stub Memory class implementation with header file

---
+++ includes/memory.h
@@ -0,0 +1,13 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <stdint.h>
+#include <array>
+
+class Memory
+{
+    private:
+        std::array<uint8_t, 0x1000> _data = {0}; 
+};
+
+#endif // MEMORY_H
