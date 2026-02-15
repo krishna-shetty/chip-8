ec0fcdda49c36f747f58392f62855780f90de29f
Krishna Shetty <KrishnaHarishShetty@gmail.com>
2026-02-15 15:49:53 -0800

    Add fontset header and load fontset into memory during initialization

--- includes/memory.cpp
+++ includes/memory.cpp
@@ -1,6 +1,10 @@
 #include "memory.h"
 
-class Memory
+Memory::Memory()
 {
-
-};
+    // Load the fontset into memory starting at address 0x050
+    for (size_t i = 0; i < FONTSET.size(); ++i)
+    {
+        _data[0x050 + i] = FONTSET[i];
+    }
+}
--- includes/memory.h
+++ includes/memory.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <array>
+#include "fontset.h"
 
 class Memory
 {
