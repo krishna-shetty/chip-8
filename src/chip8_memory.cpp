5843194140ce7cb2252a727a98b3cc2c6ceacfac
Krishna Shetty <KrishnaHarishShetty@gmail.com>
2026-02-20 20:32:38 -0800

    Implement ROM loading logic for Chip-8

--- src/chip8_memory.cpp
+++ src/chip8_memory.cpp
@@ -1,4 +1,6 @@
 #include "chip8_memory.h"
+#include "fstream"
+#include "string"
 
 using namespace chip8;
 
@@ -34,4 +36,34 @@ std::uint8_t Memory::read(std::uint16_t address) const
     {
         throw std::out_of_range("Memory address out of range");
     }
+}
+
+/*
+ * Title: Building a CHIP-* Emulator [C++]
+ * Author: Austin Morlan
+ * Date: Feb 20, 2026
+ * Source: https://austinmorlan.com/posts/chip8_emulator/
+ */
+void Memory::loadROM(uint16_t startAddress = 0x200, char const* filename)
+{
+    std::ifstream rom(filename, std::ios::binary | std::ios::ate);
+
+    if (!rom.is_open())
+    {
+        throw std::runtime_error(std::string("Error opening file: ") + filename);
+    }
+
+    std::streampos size = rom.tellg();
+    char* buffer;
+
+    rom.seekg(0, std::ios::beg);
+    rom.read(buffer, size);
+    rom.close();
+
+    for (long i = 0; i < size; ++i)
+    {
+        _data[startAddress + i] = buffer[i];
+    }
+
+    delete[] buffer;
 }
